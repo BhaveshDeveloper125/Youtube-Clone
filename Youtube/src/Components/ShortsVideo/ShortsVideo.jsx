7df2caed79bcffd266e80f4d3650f9a575867632
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-04-11 14:38:32 +0530

    small changes

--- Youtube/src/Components/ShortsVideo/ShortsVideo.jsx
+++ Youtube/src/Components/ShortsVideo/ShortsVideo.jsx
@@ -12,7 +12,7 @@ function ShortsVideo(props) {
   console.log(`btn : ${liked}`);
 
   let btn =
-    'size-14 bg-gray-100 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300';
+    'size-10 bg-gray-100 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300';
   let btnico = 'size-6 object-cover';
   return (
     <>
@@ -66,14 +66,73 @@ function ShortsVideo(props) {
         </div>
       </div> */}
 
-      <div className=" h-[90%] w-full  p-4 flex">
+      {/* <div className=" h-[90%] w-full  p-4 flex">
         <video
           src={props.v}
           controls
           className="h-full w-[83%] bg-gray-400  rounded-xl"
-        >
-          Bhavesh
-        </video>
+        />
+        <div className="flex flex-1  flex-col gap-4 justify-center items-center">
+          <button
+            onClick={() => {
+              setliked(!liked);
+              if (disliked == true) {
+                setdisliked(!disliked);
+              }
+            }}
+            className={liked ? `${btn} invert ` : `${btn}`}
+          >
+            <img src={like} alt="like" className={btnico} />
+          </button>
+          <p className=" text-sm font-semibold">1M likes</p>
+          <button
+            onClick={() => {
+              setdisliked(!disliked);
+              if (liked == true) {
+                setliked(!liked);
+              }
+            }}
+            className={disliked ? `${btn} invert ` : `${btn}`}
+          >
+            <img src={like} alt="dislike" className={`${btnico} rotate-180 `} />
+          </button>
+          <p className=" text-sm font-semibold">Dislike</p>
+          <button className={btn}>
+            <img src={comment} alt="comment" className={btnico} />
+          </button>
+          <p className=" text-sm font-semibold">999</p>
+          <button className={btn}>
+            <img src={share} alt="share" className={btnico} />
+          </button>
+          <p className=" text-sm font-semibold">Share</p>
+          <button className={`${btn} flex flex-col gap-1`}>
+            <div className="size-1 bg-black rounded-full"></div>
+            <div className="size-1 bg-black rounded-full"></div>
+            <div className="size-1 bg-black rounded-full"></div>
+          </button>
+          <div className="size-14 bg-gray-100 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300">
+            <img
+              src={cutie}
+              alt="img"
+              className="size-full rounded-full object-cover"
+            />
+          </div>
+        </div>
+      </div> */}
+
+      <div className="h-full w-full flex">
+        <div className="h-[90%] w-[85%] rounded-xl aspect-[16/9] relative">
+          <video
+            src={props.v}
+            // controls
+            className="h-full w-full rounded-xl object-cover"
+          />
+          {/* Notice : after applying the positin absolute we apply the top: 0, right: 0, bottom: 0, and left: 0 and the shorthand for the top: 0, right: 0, bottom: 0, and left: 0, is the inset-0 */}
+          <div className="h-15  absolute inset-0  bg-red-500 rounded-xl">
+            <button>Play</button>
+            <button>Volume</button>
+          </div>
+        </div>
         <div className="flex flex-1  flex-col gap-4 justify-center items-center">
           <button
             onClick={() => {
