e8ea999cac8e35431bdbf3435db741e951074479
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-04-13 17:22:28 +0530

    Small changes

--- Youtube/src/Components/ShortsVideo/ShortsVideo.jsx
+++ Youtube/src/Components/ShortsVideo/ShortsVideo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useTransition } from 'react';
+import React, { useRef, useState, useTransition } from 'react';
 
 // Importing Icons
 import like from '../../assets/Icons/like.svg';
@@ -20,7 +20,11 @@ function ShortsVideo(props) {
   const [videplay, setvidplay] = useState(false);
   const [vol, setvol] = useState(true);
   const [screen, setscreen] = useState(false);
-  console.log(`btn : ${liked}`);
+
+  // Controllers Settings and satets
+
+  const videocontrols = useRef(null);
+  const [playing, setplaying] = useState(false);
 
   let btn =
     'size-10 bg-gray-100 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300';
@@ -30,12 +34,24 @@ function ShortsVideo(props) {
       <div className="h-full w-full flex">
         <div className="h-[90%] w-[85%] rounded-xl aspect-[16/9] relative">
           <video
+            ref={videocontrols}
             src={props.v}
             className="h-full w-full rounded-xl object-cover"
           />
           {/* Notice : after applying the positin absolute we apply the top: 0, right: 0, bottom: 0, and left: 0 and the shorthand for the top: 0, right: 0, bottom: 0, and left: 0, is the inset-0 */}
           <div className="h-12 absolute inset-0 flex justify-around mt-4 rounded-xl opacity-5 hover:opacity-90">
-            <button className="h-full w-12 bg-white opacity-50 rounded-full cursor-pointer object-cover">
+            <button
+              onClick={() => {
+                if (playing) {
+                  videocontrols.current.pause();
+                  setplaying(false);
+                } else {
+                  videocontrols.current.play();
+                  setplaying(true);
+                }
+              }}
+              className="h-full w-12 bg-white opacity-50 rounded-full cursor-pointer object-cover"
+            >
               <img
                 src={videplay ? play : pause}
                 alt="play"
@@ -46,7 +62,7 @@ function ShortsVideo(props) {
 
             <div className="h-full w-[50%] bg-black opacity-50 rounded-full flex gap-2 justify-around items-center overflow-hidden  pl-2 pr-2">
               <img
-                src={vol ? volume : mute}
+                src={vol ? mute : volume}
                 alt="volume"
                 // 9 and 6
                 className="size-9 invert object-cover cursor-pointer
@@ -75,7 +91,7 @@ function ShortsVideo(props) {
           </div>
 
           <div className="h-12 absolute bottom-0 bg-red-500 flex justify-around">
-              Bhavesh
+            Bhavesh
           </div>
         </div>
         <div className="flex flex-1  flex-col gap-4 justify-center items-center">
