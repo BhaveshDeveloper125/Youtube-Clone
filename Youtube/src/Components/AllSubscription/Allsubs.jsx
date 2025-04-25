e10f10db1c808bc7fa03d77c0bc0daaef4ba7b74
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-04-25 15:32:08 +0530

    new component added

---
+++ Youtube/src/Components/AllSubscription/Allsubs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Allsubs() {
+  return (
+    <>
+      <h1>All Subs are here </h1>
+    </>
+  );
+}
+
+export default Allsubs;
--- Youtube/src/Pages/AllSubscription/AllSubscription.jsx
+++ Youtube/src/Pages/AllSubscription/AllSubscription.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 import TopMenu from '../../Components/TopMenu/TopMenu';
 import SideMenu from '../../Components/SideMenu/SideMenu';
 import Context from '../../Context/Context';
+import Allsubs from '../../Components/AllSubscription/Allsubs';
 
 function AllSubscription() {
   return (
     <>
       <Context>
         <TopMenu />
-        <SideMenu />
+        <div className="flex bg-red-500">
+          <SideMenu />
+          <div className="flex flex-1 bg-green-500 p-1j">
+            <Allsubs />
+            <Allsubs />
+            <Allsubs />
+            <Allsubs />
+            <Allsubs />
+            <Allsubs />
+            <Allsubs />
+            <Allsubs />
+            <Allsubs />
+          </div>
+        </div>
       </Context>
     </>
   );
