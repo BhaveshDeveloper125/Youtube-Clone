b5ec6d510d14895ab01334aba8df6c52b2a712a1
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-04-27 14:25:00 +0530

    user section half done

--- Youtube/src/Pages/You/You.jsx
+++ Youtube/src/Pages/You/You.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import Context from '../../Context/Context';
+import TopMenu from '../../Components/TopMenu/TopMenu';
+import SideMenu from '../../Components/SideMenu/SideMenu';
+import { Link } from 'react-router-dom';
+
+// Importing Icons
+import user from '../../assets/Icons/user.svg';
 
 function You() {
   return (
     <>
-      <h1>You Page</h1>
+      <Context>
+        <TopMenu />
+        <div className="flex">
+          <SideMenu />
+          <div className="flex flex-1 flex-col  p-2">
+            <div className="h-40 w-full flex  p-2">
+              <img
+                src={user}
+                alt="user"
+                className="size-30 object-cover rounded-full"
+              />
+              <Link className="h-20 w-100 p-2 mt-5">
+                <h1 className="text-black font-bold text-4xl">
+                  User Channel Name
+                </h1>
+                <div className=" w-80 flex justify-around">
+                  <span className="line-clamp-1">Channel ID </span>
+                  <span className="line-clamp-1">view Chanel</span>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Context>
     </>
   );
 }
