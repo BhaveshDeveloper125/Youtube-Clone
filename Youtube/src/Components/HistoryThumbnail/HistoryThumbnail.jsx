933f7b71cc8489062317fa02c08be5d9a24d8388
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-05-03 12:09:43 +0530

    links added

--- Youtube/src/Components/HistoryThumbnail/HistoryThumbnail.jsx
+++ Youtube/src/Components/HistoryThumbnail/HistoryThumbnail.jsx
@@ -11,7 +11,7 @@ function HistoryThumbnail() {
   return (
     <>
       <div className=" h-50 w-full rounded-xl flex p-2 hover:bg-gray-300">
-        <Link>
+        <Link to="/playlistPlayer/from the history section">
           <img
             src={rengoku}
             alt="thumbnail"
@@ -21,7 +21,10 @@ function HistoryThumbnail() {
 
         <div className="flex flex-col flex-1 gap-1 p-1">
           <div className=" flex gap-2 p-1">
-            <Link className=" h-15 w-110 font-bold text-lg line-clamp-2">
+            <Link
+              to="/playlistPlayer/from the history section"
+              className=" h-15 w-110 font-bold text-lg line-clamp-2"
+            >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus
               asperiores placeat harum? Voluptatem eveniet atque architecto quas
               pariatur porro ipsam dolorum repellat cumque! Ducimus natus cumque
@@ -30,7 +33,7 @@ function HistoryThumbnail() {
             <div className=" size-10 rounded-full cursor-pointer p-2 hover:bg-gray-400">
               <img src={cross} alt="cross" className="object-cover" />
             </div>
-            <div className=" h-10 w-2 flex flex-col justify-center items-center gap-1 p-1 cursor-pointer">
+            <div className=" h-fit w-3 rounded-xl flex flex-col j  ustify-center items-center gap-1 p-1 cursor-pointer active:bg-gray-400">
               <div className="size-0.5 bg-black rounded-full p-0.5"></div>
               <div className="size-0.5 bg-black rounded-full p-0.5"></div>
               <div className="size-0.5 bg-black rounded-full p-0.5"></div>
@@ -38,10 +41,15 @@ function HistoryThumbnail() {
           </div>
 
           <span className=" w-full flex item-center flex-1">
-            <Link className="">Channel Name . view channel </Link>
+            <Link to="/user/from the history section" className="">
+              Channel Name . view channel{' '}
+            </Link>
           </span>
 
-          <Link className="line-clamp-3">
+          <Link
+            to="/playlistPlayer/from the history section"
+            className="line-clamp-3"
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Non
             ratione, est provident fuga necessitatibus consequuntur nam
             consectetur nihil delectus praesentium debitis optio distinctio
