65c28cc5c20f78a41e7c9cbb08c39fcab3632322
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-06-19 23:43:25 +0530

    history section complete

--- Youtube/src/Pages/History/History.jsx
+++ Youtube/src/Pages/History/History.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Context from '../../Context/Context';
-import TopMenu from '../../Components/TopMenu/TopMenu';
-import SideMenu from '../../Components/SideMenu/SideMenu';
-import HistoryThumbnail from '../../Components/HistoryThumbnail/HistoryThumbnail';
+import React from "react";
+import Context from "../../Context/Context";
+import TopMenu from "../../Components/TopMenu/TopMenu";
+import SideMenu from "../../Components/SideMenu/SideMenu";
+import HistoryThumbnail from "../../Components/HistoryThumbnail/HistoryThumbnail";
 
 function History() {
   return (
@@ -27,9 +27,51 @@ function History() {
               </div>
             </div>
             <div className="h-full w-[40%] flex justify-center items-center p-1">
-              <div className=" h-120 w-100 bg-red-500 p-1">
-                <form action="" method="post">
-                  <input type="text" placeholder="Search Watch History" />
+              <div className=" h-120 w-100  p-1">
+                <form
+                  action=""
+                  method="post"
+                  className="flex flex-col justify-center items-center p-4"
+                >
+                  <div className="w-full  p-4 flex justify-center items-center gap-3">
+                    <i class="fas fa-magnifying-glass"></i>
+                    <input
+                      type="search"
+                      name=""
+                      id=""
+                      placeholder="Search Watch History"
+                      className="w-[80%] border-0 border-b border-black outline-none"
+                    />
+                  </div>
+                  <div className="w-full p-4 flex items-center gap-3">
+                    <i className="fas fa-trash  text-lg"></i>
+                    <button className="p-2 text-left hover:bg-gray-100 rounded">
+                      Clear all watch history
+                    </button>
+                  </div>
+                  <div className="w-full p-4 flex items-center gap-3">
+                    <i className="fas fa-pause  text-lg"></i>
+                    <button className="p-2 text-left hover:bg-gray-100 rounded">
+                      pause the watch history
+                    </button>
+                  </div>
+                  <div className="w-full p-4 flex items-center gap-3">
+                    <i className="fas fa-gear  text-lg"></i>
+                    <button className="p-2 text-left hover:bg-gray-100 rounded">
+                      Manage all history
+                    </button>
+                  </div>
+                  <div className="w-full p-4 flex items-center gap-3">
+                    <button className="pl-8 text-left hover:bg-gray-100 rounded">
+                      comments
+                    </button>
+                  </div>
+                  <div className="w-full p-4 flex items-center gap-3">
+                    <button className="pl-8 text-left hover:bg-gray-100 rounded">
+                      posts
+                    </button>
+                  </div>
+                  
                 </form>
               </div>
             </div>
