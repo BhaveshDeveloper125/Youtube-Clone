f4b0dd4fb579337d3d75706c90f2a586b44d8ed0
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-06-22 22:52:05 +0530

    playlist component is ready

--- Youtube/src/Components/PlaySection/Playsection.jsx
+++ Youtube/src/Components/PlaySection/Playsection.jsx
@@ -1,10 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// import images
+import goku from "../../assets/Images/5.jpg";
 
 function Playsection(prop) {
   return (
-    <div className="h-60 w-full rounded-2xl bg-green-500 flex justify-center items-center ">
-      <h1>Hello</h1>
-    </div>
+    <Link className="h-fit w-full rounded-xl p-2 flex flex-col items-center shadow-2xl shadow-black/50 transform translate-y-0 transition duration-300  hover:-translate-y-1 ">
+      <div className=" flex flex-col items-center relative">
+        <img
+          src={prop.img}
+          alt=""
+          className="h-2 w-[80%] rounded-t-xl  object-cover blur-[2.5px] "
+        />
+        <img
+          src={prop.img}
+          alt=""
+          className="h-2 w-[90%] rounded-t-xl  object-cover blur-[2.5px] "
+        />
+        <img
+          src={prop.img}
+          alt=""
+          className="h-50 w-full rounded-xl  object-cover"
+        />
+        <span className="bg-black rounded-md text-white absolute bottom-1 right-2">
+          <i className="fas fa-list p-2"></i>
+          <span>10 videos</span>
+        </span>
+      </div>
+      <div className="h-fit w-full p-1 mt-2 ">
+        <h1 className="line-clamp-2">{prop.title}</h1>
+        <div className="flex">
+          <h1 className="w-[50%] truncate text-center p-2">
+            {prop.channelName}
+          </h1>
+          <h1 className="w-[50%] text-center p-2">playlist</h1>
+        </div>
+        <Link href="">view full playlist</Link>
+      </div>
+    </Link>
   );
 }
 
--- Youtube/src/Pages/PlayList/Playlist.jsx
+++ Youtube/src/Pages/PlayList/Playlist.jsx
@@ -6,7 +6,7 @@ import PlaylistComponent from "../../Components/Playlist/PlaylistComponent";
 import Playsection from "../../Components/PlaySection/Playsection";
 
 // Import Images
-import goku from "../../assets/Images/5.jpg";
+import goku from "../../assets/Images/1.jpeg";
 
 function Playlist() {
   return (
@@ -15,76 +15,90 @@ function Playlist() {
         <TopMenu />
         <div className="flex">
           <SideMenu />
-          {/* <div className="bg-green-500 flex-1 grid gap-2 justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
-          </div> */}
-
-          {/* <section class="   flex-1 p-8 transition-all duration-300 ease-in-out ">
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-10">
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-
-              <div class="h-50 w-full bg-white rounded-xl shadow-md overflow-hidden transition-transform duration-300 hover:-translate-y-1 hover:shadow-lg">
-                hello
-              </div>
-            </div>
-          </section> */}
           <div
-            className=" flex-1 bg-red-500 grid row-gap-1 gap-x-3 grid-cols-1 sm:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 xl:grid-col-5
-           2xl:grid-cols-6"
+            className=" p-4 flex-1 grid gap-x-4 gap-y-4  grid-cols-1 sm:grid-col-2 lg:grid-cols-3 xl:grid-cols-4 xl:grid-col-5
+           2xl:grid-cols-6 overflow-x-auto"
           >
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
-            <Playsection />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
+            <Playsection
+              img={goku}
+              title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+              channelName="Channel Title : This is the channel title"
+            />
           </div>
         </div>
       </Context>
