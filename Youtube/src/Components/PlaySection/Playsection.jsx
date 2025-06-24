9c92aa3a7a70caec5a86f905904ffb924454a2ef
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-06-24 19:24:26 +0530

    new file is added

--- Youtube/src/Components/PlaySection/Playsection.jsx
+++ Youtube/src/Components/PlaySection/Playsection.jsx
@@ -7,7 +7,7 @@ import goku from "../../assets/Images/5.jpg";
 function Playsection(prop) {
   return (
     <Link className="h-fit w-full rounded-xl p-2 flex flex-col items-center shadow-2xl shadow-black/50 transform translate-y-0 transition duration-300  hover:-translate-y-1 ">
-      <div className="h-fit w-full flex flex-col items-center relative">
+      <div className="h-fit w-full flex flex-col items-center relative group">
         <img
           src={prop.img}
           alt=""
@@ -27,6 +27,13 @@ function Playsection(prop) {
           <i className="fas fa-list p-2"></i>
           <span>10 videos</span>
         </span>
+        <Link
+          to={`/playlists/${prop.plink}`}
+          className="size-full bg-black/50 rounded-xl text-white group-hover:flex gap-2 justify-center items-center absolute hidden"
+        >
+          <i className="fas fa-play"></i>
+          Play all
+        </Link>
       </div>
       <div className="h-fit w-full p-1 mt-2 ">
         <h1 className="line-clamp-2">{prop.title}</h1>
@@ -36,7 +43,7 @@ function Playsection(prop) {
           </h1>
           <h1 className="w-[50%] text-center p-2">playlist</h1>
         </div>
-        <Link href="">view full playlist</Link>
+        <Link to={`/playlistPlayer/${prop.lnk}`}>view full playlist</Link>
       </div>
     </Link>
   );
--- Youtube/src/Pages/PlayList/Playlist.jsx
+++ Youtube/src/Pages/PlayList/Playlist.jsx
@@ -23,82 +23,118 @@ function Playlist() {
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={1}
+              plink={1}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={2}
+              plink={2}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={3}
+              plink={1}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={4}
+              plnk={4}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={5}
+              plnk={5}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={6}
+              plnk={6}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={7}
+              plnk={7}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={8}
+              plnk={8}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={9}
+              plnk={9}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={10}
+              plnk={10}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={11}
+              plnk={11}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={12}
+              plnk={12}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={13}
+              plnk={13}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={14}
+              plnk={14}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={15}
+              plnk={15}
             />
             <Playsection
               img={goku}
               title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
               channelName="Channel Title : This is the channel title"
+              lnk={16}
+              plnk={16}
             />
+            <br />
+            <br />
+            <br />
+            <br />
           </div>
         </div>
       </Context>
