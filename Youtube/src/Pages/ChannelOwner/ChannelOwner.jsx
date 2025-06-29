aa8b1b050e884e87368c7a116330802741f751b1
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-06-29 18:03:29 +0530

    working on channel owner page is start

--- Youtube/src/Pages/ChannelOwner/ChannelOwner.jsx
+++ Youtube/src/Pages/ChannelOwner/ChannelOwner.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import Context from "../../Context/Context";
+import TopMenu from "../../Components/TopMenu/TopMenu";
+import SideMenu from "../../Components/SideMenu/SideMenu";
+import { useParams } from "react-router-dom";
+
+// import Images
+import rengoku from "../../assets/Images/rengoku.jpg";
 
 function ChannelOwner() {
   const params = useParams();
   return (
     <>
-      <h1>The Page of Youtuber You Subscribed</h1>
-      <h1>Channel Owner Pager {params.id}</h1>
+      <Context>
+        <TopMenu />
+        <div className="flex">
+          <SideMenu />
+          <div className="flex justify-center items-center flex-1">
+            <div className="h-screen w-[85%]  ">
+              <img
+                src={rengoku}
+                alt="channel banner"
+                className="h-60 w-full rounded-3xl object-cover"
+              />
+
+              {/* <h1>The Page of Youtuber You Subscribed</h1>
+              <h1>Channel Owner Pager {params.id}</h1> */}
+            </div>
+          </div>
+        </div>
+      </Context>
     </>
   );
 }
