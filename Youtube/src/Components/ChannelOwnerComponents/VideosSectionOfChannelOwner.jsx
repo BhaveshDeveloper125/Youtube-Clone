3ae85e15920400cb4354d9247de237cf8e16cc28
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-07-13 10:48:39 +0530

    started working on the videos setion

--- Youtube/src/Components/ChannelOwnerComponents/VideosSectionOfChannelOwner.jsx
+++ Youtube/src/Components/ChannelOwnerComponents/VideosSectionOfChannelOwner.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import WatchLaterVideo from "../WatchLaterVideo/WatchLaterVideo";
+
+// Importing Images
+import cutie from "../../assets/Images/1.jpeg";
 
 function VideosSectionOfChannelOwner() {
   return (
     <>
-      <h1>Videos Section</h1>
+      <div className="flex-1 ">
+        <div className="w-full p-4 flex gap-4">
+          <button className="bg-gray-200 rounded-xl p-2 focus:bg-black focus:text-white cursor-pointer">
+            Latest
+          </button>
+          <button className="bg-gray-200 rounded-xl p-2 focus:bg-black focus:text-white cursor-pointer">
+            Popular
+          </button>
+          <button className="bg-gray-200 rounded-xl p-2 focus:bg-black focus:text-white cursor-pointer">
+            Oldest
+          </button>
+        </div>
+        <div className="bg-green-500">
+          <WatchLaterVideo
+            img={cutie}
+            title=" Most Viewed videos : video title this is the video title this is the video title  this is the video titlethis is the video titlethis is the video titlethis is the video title"
+            channelName="Channel Name Channel Name Channel Name Channel Name "
+            views="1M"
+            time="1 year"
+          />
+        </div>
+      </div>
     </>
   );
 }
