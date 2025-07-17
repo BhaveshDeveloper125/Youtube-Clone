e5ba324e4a02bf2fa33fd23baae3388899137b08
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-07-17 23:32:43 +0530

    playlist section of the channel owner page

--- Youtube/src/Components/ChannelOwnerComponents/PlaylistsSectionOfChannelOwner.jsx
+++ Youtube/src/Components/ChannelOwnerComponents/PlaylistsSectionOfChannelOwner.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import PlaySection from "../../Components/PlaySection/Playsection";
+
+// import images
+import goku from "../../assets/Images/5.jpg";
 
 function PlaylistsSectionOfChannelOwner() {
   return (
     <>
-      <h1>Playlist section</h1>
+      <div className="grid gap-1 grid-cols-4">
+        {[...Array(4)].map((i) => (
+          <PlaySection
+            key={i}
+            img={goku}
+            title="This is the title ipsum dolor sit, amet consectetur adipisicing elit. Voluptates deleniti explicabo quam. Repellat cumque possimus, aperiam illum dicta tempora sequi excepturi amet impedit iusto sint molestias alias nemo praesentium officia."
+            channelName="Channel Title : This is the channel title"
+            lnk={1}
+            plink={1}
+          />
+        ))}
+      </div>
     </>
   );
 }
